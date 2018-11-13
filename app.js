379ff2b554a8129875174b46ced2611de5406232
Ionut Elian Tudor <ionuteliantudor@gmail.com>
2018-11-13 04:30:15 +0200

    Finally Working

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ var phoneNumber           = document.querySelector('.phoneNumber');
 var validateNumber        = document.querySelector('#validateNumber');
 var rezultat              = document.querySelector('#result');
 var boxForNumbers         = document.getElementById('numbersInput');
-var viewNumber            = document.querySelector('#viewNumber');
 var numereDeTelefonValide = [];
 
 
 rezultat.addEventListener("click", stergeRezultat);
 validateNumber.addEventListener("click", valideazaNumarTelefon);
-viewNumber.addEventListener("click", showAllNumbers);
 
 function valideazaNumarTelefon(){
         var newPhoneNumber = phoneNumber.value;
@@ -16,30 +14,34 @@ function valideazaNumarTelefon(){
             rezultat.classList.remove('result-valid');
             rezultat.innerHTML = "Numarul este invalid, ati introdus litere!";
             rezultat.classList.add('result-invalid');
+            phoneNumber.value = "";
         }else if(newPhoneNumber.length < 10){
             rezultat.classList.remove('result-valid');
             rezultat.innerHTML = "<p>Ati introdus " +  newPhoneNumber.length + " cifre." + "</p><p>Numarul introdus nu are suficiente cifre, aveti nevoie de minim 10!</p>";
             rezultat.classList.add('result-invalid');
+            phoneNumber.value = "";
         }else if(newPhoneNumber.length > 10){
             rezultat.classList.remove('result-valid');
             rezultat.innerHTML = "<p>Ati introdus " + newPhoneNumber.length + " cifre"+ " </p><p>Numarul maxim de cifre pentru un numar valid este 10.</p>";
             rezultat.classList.add('result-invalid');
+            phoneNumber.value = "";
         }else if(newPhoneNumber.length == 10) {
             rezultat.classList.remove('result-invalid');
             rezultat.innerHTML = "Numarul introdus este valid! Felicitari";
             rezultat.classList.add('result-valid');      
             numereDeTelefonValide.push(newPhoneNumber);
             phoneNumber.value = "";
-            console.log(numereDeTelefonValide);
+            showAllNumbers();
         }
 }
+
 function showAllNumbers(){
-    for(var i = 1; i < numereDeTelefonValide.length; i++){
+    for(var i = 0; i < numereDeTelefonValide.length; i++){
         var newInput        = document.createElement('input');
         newInput.setAttribute("class", "phoneNumber");
         newInput.setAttribute("value", numereDeTelefonValide[i]);
-        boxForNumbers.appendChild(newInput);
     }
+    boxForNumbers.appendChild(newInput);
 }
 
 //Sterge rezultatul intors indiferent daca este valid sau nu
